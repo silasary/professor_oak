702f1710080d770354e270cdfaf17b11f708f666
Katelyn Gigante <clockwork.singularity@gmail.com>
2020-08-24 02:39:04 +1000

    update.py is currently useless

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ cd $(dirname $0)
 git fetch
 git reset --hard origin/`git rev-parse --abbrev-ref HEAD`
 pipenv install
-pipenv run python update.py
+# pipenv run python update.py
 pipenv run python run.py
